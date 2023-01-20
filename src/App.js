306d08215ce88678ad5b97820bcc652e2670f12c
Sushmitacode12 <sushmitatripathi786@gmail.com>
2023-01-20 21:48:02 +0530

    Cart Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Fragment } from "react";
+
 import Header from "./Layout/Header";
 import Meals from "./Meals/Meals";
+import Cart from "./Cart/Cart";
 
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
