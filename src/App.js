a2bd3ace50aa03e51cc244c38730666572104a3a
Sushmitacode12 <sushmitatripathi786@gmail.com>
2023-01-20 18:24:48 +0530

    Summary added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { Fragment } from "react";
 import Header from "./Layout/Header";
+import Meals from "./Meals/Meals";
 
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
-}
+};
 
 export default App;
